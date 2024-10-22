3678f9f23a3020738410ef1b2ede79e044080abb
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-10-22 08:54:30 -0500

    style: :lipstick: Se completo la vista del  LogIn

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import AppRouter from '../router/RouterApp.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import '../styles/Notes.module.css'
+import '../styles/LogIn.css'
 import '../styles/NotesCard.module.css'
 
 createRoot(document.getElementById('root')).render(
--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Notes } from '../components/Notes';
 import { Cards } from '../components/AllNotes';
+import { Login } from '../components/LogIn';
 
 export const AppRouter = () => {
     return(
         <Routes>
-            <Route path='/' element={<Notes/>}/>
+            <Route path='/home' element={<Notes/>}/>
             <Route path='/cards' element={<Cards/>}/>
+            <Route path='/' element={<Login/>}/>
         </Routes>
     )
 }
