6373029d69aac8632fc611accdd956d75124d186
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-10-23 07:24:58 -0500

    style: :lipstick: Se terminaron los ajustes de diseño en ContentNote

--- src/components/ContentNote.jsx
+++ src/components/ContentNote.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/ContentNote.module.css';
 
 export const ContentNote = () => {
+    const [title, setTitle] = useState('Title');
+    const [value, setValue] = useState('');
+
+    const handleFocus = () => {
+        setValue('');
+    };
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
     return (
         <main className={styles['content-editor']}>
-        <section className={styles['content-wrapper']}>
-            <header className={styles['header-container']}>
-            <div className={styles['text-container']}>
-                <button className={styles['action-button']} aria-label="Menu" />
-                <h1 className={styles['keyboard-title']}>Title</h1>
-                <input type="text" className={styles['input-placeholder']} placeholder="Type something..." />
-            </div>
-            <div className={styles['button-group']}>
-                <button className={styles['action-button']} aria-label="Action 1" />
-                <button className={styles['action-button']} aria-label="Action 2" />
-            </div>
-            </header>
-        </section>
+            <section className={styles['content-wrapper']}>
+                <header className={styles['header-container']}>
+                    <div className={styles['text-container']}>
+                        <button className={styles['action-button']} aria-label="Menu" />
+                        <input type="text" className={styles['content-title']} value={title} onChange={handleTitleChange} 
+                        />
+                        <input type="text" className={styles['input-placeholder']} placeholder="Type something..." value={value} onFocus={handleFocus} onChange={handleChange} 
+                        />
+                    </div>
+                    <div className={styles['button-group']}>
+                        <button className={styles['action-button']} aria-label="Action 1" />
+                        <button className={styles['action-button']} aria-label="Action 2" />
+                    </div>
+                </header>
+            </section>
         </main>
     );
-}
+};
 
 export default ContentNote;
