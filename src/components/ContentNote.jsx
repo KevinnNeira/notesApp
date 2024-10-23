e8030ef35cbcde3cf7b0673e6dc1e1914c90f899
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-10-23 08:34:57 -0500

    style: :lipstick: Se insertaron las imagenes necesarias para la vista de ContentNote.jsx

--- src/components/ContentNote.jsx
+++ src/components/ContentNote.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import styles from '../styles/ContentNote.module.css';
+import save from '../../public/save.svg'
+import visibility from '../../public/visibility.svg'
+import back from '../../public/Vector.svg'
 
 export const ContentNote = () => {
     const [title, setTitle] = useState('Title');
@@ -22,15 +25,22 @@ export const ContentNote = () => {
             <section className={styles['content-wrapper']}>
                 <header className={styles['header-container']}>
                     <div className={styles['text-container']}>
-                        <button className={styles['action-button']} aria-label="Menu" />
+                    <button className={styles['action-button']} aria-label="Menu">
+                        <img id='image__menu__note' src={back} alt="Menu" />
+                    </button>
+
                         <input type="text" className={styles['content-title']} value={title} onChange={handleTitleChange} 
                         />
                         <input type="text" className={styles['input-placeholder']} placeholder="Type something..." value={value} onFocus={handleFocus} onChange={handleChange} 
                         />
                     </div>
                     <div className={styles['button-group']}>
-                        <button className={styles['action-button']} aria-label="Action 1" />
-                        <button className={styles['action-button']} aria-label="Action 2" />
+                    <button className={styles['action-button']} aria-label="Action 1">
+                        <img src={visibility} alt="Descripción 1" />
+                    </button>
+                    <button className={styles['action-button']} aria-label="Action 2">
+                        <img src={save} alt="Descripción 2" />
+                    </button>
                     </div>
                 </header>
             </section>
