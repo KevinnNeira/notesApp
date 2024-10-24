85ea84cb26503d8184791944a851583aa1979e4d
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-10-24 06:55:18 -0500

    feat: :sparkles: Se realizo el endpoint llamado /insertUser para crear un nuevo usuario

--- api/server/server.js
+++ api/server/server.js
@@ -14,6 +14,30 @@ user.get('/getNotes', async (req, res) => {
     res.status(200).json(await collection.find().project().toArray())
 });
 
+user.post('/insertUser', express.json(), async (req, res) => {
+    const db = await connectMongodb();
+    const collection = db.collection('usuarios')
+    const { Username, Email, Password } = req.body;
+
+    if (!Username || !Email || !Password) {
+        return res.status(400).json({ message: "Username, Email and Password are required" });
+    }
+
+    const newUser = {
+        Username,
+        Email,
+        Password
+    };
+
+    try {
+        const result = await collection.insertOne(newUser);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "User not created" });
+    }
+});
+
 let config = { 
     port: "3000",
     host: "localhost"
