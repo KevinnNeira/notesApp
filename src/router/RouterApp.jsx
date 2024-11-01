c231826979dc9a6215b9e3c0be5718840581d94f
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-11-01 08:09:53 -0500

    fix: :truck: Se hicieron cambios en el router

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -7,7 +7,7 @@ import { SignUp } from '../components/SignUp';
 export const AppRouter = () => {
     return(
         <Routes>
-            <Route path='/' element={<SignUp/>}/>
+            <Route path='' element={<SignUp/>}/>
             <Route path='/login' element={<Login/>}/>
             <Route path='/home' element={<Notes/>}/>
             <Route path='/content' element={<ContentNote/>}/>
